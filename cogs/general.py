c1af1ed2870d8a0151ad66f17cfb88cbca37183d
TitusKirch <requests@tkirch.dev>
2019-09-27 21:02:55 +0200

    Add info command with general informations about the bot

--- cogs/general.py
+++ cogs/general.py
@@ -9,5 +9,11 @@ class General(commands.Cog, name='General'):
     async def help(self, ctx):
         await ctx.send('>>> Coming soon')
 
+    @commands.command(aliases=['i'])
+    async def info(self, ctx):
+        await ctx.send('The UninterestingBot is a self hosting Discord-Bot, written by Uninteresting Development in Python.\n\n' +
+                        'Website: https://uninteresting.dev/\n' + 
+                        'GitHub Repository: https://github.com/uninterestingDev/uninteresting-bot\n')
+
 def setup(bot):
     bot.add_cog(General(bot))
