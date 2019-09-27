93db65d733294e702414c0b18b044f0b044857dc
TitusKirch <requests@tkirch.dev>
2019-09-27 19:21:24 +0200

    Add basic admin cog for load/unload/reload extensions

---
+++ cogs/admin.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+
+class Admin(commands.Cog, name='Admin'):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def load(self, ctx, module: str):
+        try:
+            self.bot.load_extension(module)
+        except Exception as e:
+            await ctx.send(':x: "' + module + '" was not loaded. The following error message occurred:')
+            await ctx.send('```{}: {}```'.format(type(e).__name__, e))
+        else:
+            await ctx.send(':white_check_mark: "' + module + '" was successfully loaded.')
+
+    @commands.command()
+    async def unload(self, ctx, module : str):
+        try:
+            self.bot.unload_extension(module)
+        except Exception as e:
+            await ctx.send(':x: "' + module + '" was not unloaded. The following error message occurred:')
+            await ctx.send('```{}: {}```'.format(type(e).__name__, e))
+        else:
+            await ctx.send(':white_check_mark: "' + module + '" was successfully unloaded.')
+
+    @commands.command()
+    async def reload(self, ctx, module : str):
+        try:
+            self.bot.unload_extension(module)
+            self.bot.load_extension(module)
+        except Exception as e:
+            await ctx.send(':x: "' + module + '" was not reloaded. The following error message occurred:')
+            await ctx.send('```{}: {}```'.format(type(e).__name__, e))
+        else:
+            await ctx.send(':white_check_mark: "' + module + '" was successfully reloaded.')
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
