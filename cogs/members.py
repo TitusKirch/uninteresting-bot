529c04d5db5b0973aedf7641ef706cdbf7c65cae
TitusKirch <requests@tkirch.dev>
2020-01-17 14:53:12 +0100

    feat: improve code

--- cogs/members.py
+++ cogs/members.py
@@ -9,15 +9,15 @@ class Members(commands.Cog, name='Members'):
     
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        if self.config['guild']['history_channel'] > 0:
-            channel = member.guild.get_channel(self.config['guild']['history_channel'])
+        if int(self.config['guild']['history_channel']) > 0:
+            channel = member.guild.get_channel(int(self.config['guild']['history_channel']))
             if channel != None:
                 await channel.send('**Welcome {0.mention} :tada:**'.format(member))
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        if self.config['guild']['history_channel'] > 0:
-            channel = member.guild.get_channel(self.config['guild']['history_channel'])
+        if int(self.config['guild']['history_channel']) > 0:
+            channel = member.guild.get_channel(int(self.config['guild']['history_channel']))
             if channel != None:
                 await channel.send('*Bye {0.mention} :sleepy:*'.format(member))
 
