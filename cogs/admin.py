1b00dc4dbb7939c425e606e3d9687f6238f5ece5
TitusKirch <requests@tkirch.dev>
2020-01-18 15:26:52 +0100

    feat: add placeholder roles feature

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,9 +1,11 @@
 import discord
 from discord.ext import commands
+from utilities import getConfig
 
 class Admin(commands.Cog, name='Admin'):
     def __init__(self, bot):
         self.bot = bot
+        self.config = getConfig()
         self.cogs_dir = 'cogs.'
 
     @commands.command()
@@ -39,6 +41,27 @@ class Admin(commands.Cog, name='Admin'):
             await ctx.send('```{}: {}```'.format(type(e).__name__, e))
         else:
             await ctx.send(':white_check_mark: "' + module + '" was successfully reloaded.')
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def setplaceholders(self, ctx):
+        await ctx.send(':clock10: An attempt is made to set all placeholder roles. This may take a few minutes.')
+        try:
+            if self.config['guild']['use_placeholders'] != "0":
+                for guild in self.bot.guilds:
+                    for member in guild.members:
+                        tmpRoles = []
+                        for role in member.roles:
+                            tmpRoles.append(int(role.id))
+                        for placeholder in self.config['guild']['use_placeholders'].split(','):
+                            if int(placeholder) not in tmpRoles:
+                                role = discord.utils.get(guild.roles, id=int(placeholder))
+                                await member.add_roles(role)
+        except Exception as e:
+            await ctx.send(':x: Somthing went wrong. The following error message occurred:')
+            await ctx.send('```{}: {}```'.format(type(e).__name__, e))
+        else:
+            await ctx.send(':white_check_mark: All placeholder roles was successfully set.')
 
 def setup(bot):
     bot.add_cog(Admin(bot))
--- cogs/members.py
+++ cogs/members.py
@@ -13,6 +13,10 @@ class Members(commands.Cog, name='Members'):
             channel = member.guild.get_channel(int(self.config['guild']['history_channel']))
             if channel != None:
                 await channel.send('**Welcome {0.mention} :tada:**'.format(member))
+        if self.config['guild']['use_placeholders'] != "0":
+            for placeholder in self.config['guild']['use_placeholders'].split(','):
+                role = discord.utils.get(guild.roles, id=int(placeholder))
+                await member.add_roles(role)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
