f67e55ee7fc55c9d62d64e74c1e5c4e0bffcb579
TitusKirch <requests@tkirch.dev>
2020-01-24 20:10:42 +0100

    feat: revise the complete bot

--- bot.py
+++ bot.py
@@ -3,73 +3,61 @@ import logging
 from datetime import datetime
 from utilities import getConfig
 from discord.ext import commands
-import asyncio
-import requests
-
-extensions = (
-    'cogs.admin',
-    'cogs.general',
-    'cogs.members',
-    'cogs.rules',
-    'cogs.specialroles',
-    'cogs.social'
-)
+from models.setting import Setting
+from db import db_session
 
 class UninterestingBot(commands.AutoShardedBot):
     def __init__(self):
         # get config
         self.config = getConfig()
 
-        #setup logger if logging is true
-        if(self.config['bot']['logging'].lower() == 'true'):
-            logger = logging.getLogger('discord')
-            logger.setLevel((logging.DEBUG if self.config['bot']['debug'].lower() == 'true' else logging.ERROR))
+        # setup logger if logging is true
+        if(self.config['bot']['logging'] == 'true'):
+            # get loggers
+            self.discordLogger = logging.getLogger('discord')
+            self.sqlalchemyLogger = logging.getLogger('sqlalchemy.engine')
+
+            # set debug levels
+            self.discordLogger.setLevel((logging.DEBUG if self.config['bot']['debug'] == 'true' else logging.ERROR))
+            self.sqlalchemyLogger.setLevel((logging.DEBUG if self.config['bot']['debug'] == 'true' else logging.ERROR))
+            
+            # setup handler
             handler = logging.FileHandler(filename=datetime.now().strftime('logs/log%Y-%m-%d_%H-%M-%S.log'), encoding='utf-8', mode='w')
             handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-            logger.addHandler(handler)
+
+            # add handlers
+            self.discordLogger.addHandler(handler)
+            self.sqlalchemyLogger.addHandler(handler)
 
         # setup client
         super().__init__(command_prefix=self.config['bot']['command_prefix'], case_insensitive=True)
-
-        # remove default commands
-        self.remove_command('help')
-        
-        # load extensions
-        for extension in extensions:
-            try:
-                self.load_extension(extension)
-            except Exception as e:
-                print(e)
     
     async def on_ready(self):
-        if self.config['bot']['status_name'] != '':
-            await self.change_presence(activity=discord.Activity(
-                name=self.config['bot']['status_name'],
-                type=discord.ActivityType.playing))
-        print('Logged in as "' + self.user.name + '" [ID: ' + str(self.user.id) + ']')
-    
-        # check if cachet url isset
-        if self.config['cachet']['url'] not in ["", "myUrl"]:
-            # check if api_token isset
-            if self.config['cachet']['api_token'] not in ["", "myToken"]:
-                # check component id
-                if int(self.config['cachet']['component_id']) > 0:
-                    # loop status
-                    self.loop.create_task(self.cachet_status())
-    
-    async def cachet_status(self):
-        while True:
-            try:
-                headers = {'content-type': 'application/json', 'X-Cachet-Token': self.config['cachet']['api_token'], 'User-Agent': 'Mozilla/5.0'}
-                payload = "{\"status\": " + str(1) + ", \"meta\":{\"time\": \"" + str(int(datetime.timestamp(datetime.now()))) + "\"}}"
-                path = "/api/v1/components/" + self.config['cachet']['component_id']
-                requests.request("PUT", self.config['cachet']['url'] + path, data=payload, headers=headers)
-            except:
-                pass
-            await asyncio.sleep(int(self.config['cachet']['interval']))
+        # log logged in if logging is true
+        if(self.config['bot']['logging'] == 'true'):
+            self.discordLogger.info('Logged in as "' + self.user.name + '" [ID: ' + str(self.user.id) + ']')
+
+        # set presence
+        await self.change_presence(activity=discord.Activity(
+            name=Setting.get('status_name'),
+            type=discord.ActivityType.playing))
     
     def run(self):
+        # try to run bot
         try:
+            # print for debug
+            print('Bot is starting')
+
+            # run bot
             super().run(self.config['bot']['token'], reconnect=True)
-        except Exception as e:
-            print(e)
+
+        except Exception:
+            # print for debug
+            print('Bot stopped')
+
+            # close bot
+            self.close()
+
+            # log exception if logging is true
+            if(self.config['bot']['logging'] == 'true'):
+                self.discordLogger.exception('Bot run exception')
