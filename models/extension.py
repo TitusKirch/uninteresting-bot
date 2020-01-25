7e100755e2d945bffddcbd1c720bbc0fad2cf940
TitusKirch <requests@tkirch.dev>
2020-01-25 01:25:34 +0100

    feat: create basic extensions model

---
+++ models/extension.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, String, Boolean
+from models.base import Base
+import sqlalchemy
+from db import db_session
+
+class Extension(Base):
+    __tablename__ = 'extensions'
+    
+    name = Column(String, primary_key=True)
+    isLoaded = Column(Boolean, server_default=u'false')
+
+    def __init__(self, name, isLoaded):
+        self.name = name
+        self.isLoaded = isLoaded
+
+    @classmethod
+    def get(cls, name):
+        return db_session.query(Extension).filter(Extension.name==name).first()
+
+    @classmethod
+    def loaded(cls):
+        return db_session.query(Extension).filter(Extension.isLoaded==1).all()
+
+    @classmethod
+    def unloaded(cls):
+        return db_session.query(Extension).filter(Extension.isLoaded==0).all()
